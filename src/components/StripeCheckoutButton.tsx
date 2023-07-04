b052e6d8f725c6735253f438fd15f915c073ae97
Hewlbern <michael.t.holborn@gmail.com>
2023-07-04 17:50:35 +1000

    latest update

--- src/components/StripeCheckoutButton.tsx
+++ src/components/StripeCheckoutButton.tsx
@@ -3,28 +3,26 @@ import React from 'react';
 import { Button } from "@canva/app-ui-kit";
 import { createCheckoutSession, redirectToCheckout } from './stripe/stripeService';
 
-interface StripeCheckoutButtonProps {
-  onPaymentStatus: (status: string) => void;
-}
+interface StripeCheckoutButtonProps {}
 
-const StripeCheckoutButton: React.FC<StripeCheckoutButtonProps> = ({ onPaymentStatus }) => {
-  const handleClick = async () => {
-    try {
-      const session = await createCheckoutSession();
-      const result = await redirectToCheckout(session.id);
-
-      if (result.error) {
-        onPaymentStatus(result.error.message);
-      }
-    } catch (error) {
-      console.error("Failed to checkout:", error);
-    }
+const StripeCheckoutButton: React.FC<StripeCheckoutButtonProps> = () => {
+  const handleClick = (event: any) => {
+    event.preventDefault();
+    createCheckoutSession()
+      .then(session => {
+        redirectToCheckout(session.url);
+      })
+      .catch(error => {
+        console.error("Failed to checkout:", error);
+      });
   };
 
   return (
-    <Button variant="primary" onClick={handleClick}>
-      Checkout
-    </Button>
+    <form action="/create-checkout-session" method="POST">
+      <Button variant="primary" onClick={handleClick}>
+        Checkout
+      </Button>
+    </form>
   );
 };
 
--- src/components/stripe/stripeService.tsx
+++ src/components/stripe/stripeService.tsx
@@ -1,23 +1,11 @@
 // stripeService.ts
-import { loadStripe } from '@stripe/stripe-js';
-
-// Replace with your own Publishable Key
-const PUBLISHABLE_KEY = 'pk_test_1234';
-
-let stripePromise;
-export const getStripe = () => {
-  if (!stripePromise) {
-    stripePromise = loadStripe(PUBLISHABLE_KEY);
-  }
-  return stripePromise;
-};
-
 export const createCheckoutSession = async () => {
-  const response = await fetch('/create-checkout-session', { method: 'POST' });
-  return await response.json();
-};
-
-export const redirectToCheckout = async (sessionId: string) => {
-  const stripe = await getStripe();
-  return await stripe.redirectToCheckout({ sessionId });
-};
+    const response = await fetch('https://nftmintpayment-ymnzh6rdlq-uc.a.run.app/create-checkout-session', { method: 'POST' });
+    const data = await response.json();
+    return data;
+  };
+  
+  export const redirectToCheckout = (url: string) => {
+    window.location.href = url;
+  };
+  
