4ce04771fe29fc69018fabca012b56fceebebc10
Hewlbern <michael.t.holborn@gmail.com>
2023-07-25 16:49:06 +1000

    bug

--- src/components/authentication/auth0Auth.tsx
+++ src/components/authentication/auth0Auth.tsx
@@ -1,5 +1,5 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { useAppState } from "src/useAppState";
+import useAppState from "../../useAppState";
 
 export const useAuth0Auth = () => {
   const { updateState } = useAppState();
