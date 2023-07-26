6b31e104d8b27ebf0214c626db2ba53c7b5272da
Hewlbern <michael.t.holborn@gmail.com>
2023-07-27 00:37:21 +1000

    update

--- src/components/authentication/useAuthentication.test.tsx
+++ src/components/authentication/useAuthentication.test.tsx
@@ -1,149 +1,92 @@
-import React from 'react'; 
-// Import React rendering methods and helper functions from the React Testing Library
-import { render, act, waitFor } from '@testing-library/react';
-
-// Import the necessary hooks and functions from your modules
-import { useAuthentication } from './useAuthentication';
+import { renderHook, act } from '@testing-library/react';
 import { initiateCanvaAuthenticationFlow } from './canvaAuth';
 import { useAuth0Auth } from './auth0Auth';
 import useAppState from '../../useAppState';
+import { useAuthentication } from './useAuthentication';
 
-// Import jest for mocking
-import { jest } from '@jest/globals';
+// Mock the dependencies
+jest.mock('./canvaAuth');
+jest.mock('./auth0Auth');
+jest.mock('../../useAppState');
 
-interface TestCase {
-  name: string;
-  authState: null | { user: { id: string; name: string; }; tokens: {} };
-  canvaAuth: boolean;
-  auth0Auth: boolean;
-  expectedIsAuthenticating: boolean;
-  getExpectedAuthState: (auth0Token?: string) => any;
-}
+// Mock the localStorage
+const mockLocalStorage = {
+  length: 0,
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  clear: jest.fn(),
+  removeItem: jest.fn(),
+  key: jest.fn()
+};
+global.localStorage = mockLocalStorage;
 
-// Mocking
-// Mock the Canva authentication function
-jest.mock('./canvaAuth', () => ({
-  initiateCanvaAuthenticationFlow: jest.fn(),
-}));
-
-// Mock the Auth0 authentication hook
-jest.mock('./auth0Auth', () => ({
-  useAuth0Auth: jest.fn(),
-}));
-
-// Mock the app state hook
-jest.mock('../../useAppState', () => ({
-  useAppState: jest.fn(),
-}));
-
-// Define a test component that uses the authentication hook 
-// This will be used in our render function to assert against the output of the hook
-function TestComponent(props) {
-  const auth = useAuthentication();
-
-  // Render two divs containing the results of the hook
-  return (
-    <div>
-      <div>Authenticating: {auth.isAuthenticating.toString()}</div>
-      <div>Auth State: {auth.isAuthenticating}</div>
-    </div>
-  );
-}
-
-// Helper function to set auth state in localStorage
-function setAuthStateInLocalStorage(authState) {
-  localStorage.setItem('auth', JSON.stringify(authState));
-}
-
-// Helper function to clear localStorage
-function clearLocalStorage() {
-  localStorage.clear();
-}
-
-// Begin the test suite for the useAuthentication hook
 describe('useAuthentication', () => {
-  // Before each test, clear the localStorage to ensure a clean testing environment
   beforeEach(() => {
-    clearLocalStorage();
+    jest.resetAllMocks();
+    jest.useFakeTimers();
   });
 
-  const testCases: TestCase[] = [
+  afterEach(() => {
+    jest.runOnlyPendingTimers();
+    jest.useRealTimers();
+  });
+
+  const testCases = [
     {
-      name: 'when local storage has auth state, should not be authenticating',
-      authState: { user: { id: '1', name: 'John Doe' }, tokens: {} },
-      canvaAuth: true,
-      auth0Auth: true,
-      expectedIsAuthenticating: false,
-      getExpectedAuthState: (auth0Token) => ({ auth: { isAuthenticated: true, data: { token: auth0Token } } }),
+      description: 'should authenticate with local storage data',
+      localStorageData: { token: 'mock-token', user: 'mock-user' },
+      isAuthenticating: false
     },
     {
-      name: 'when local storage is empty and Canva authentication fails, should not be authenticating',
-      authState: null,
+      description: 'should authenticate with Canva and Auth0 when no local storage data',
+      localStorageData: null,
+      canvaAuth: true,
+      isAuthenticating: false
+    },
+    {
+      description: 'should not authenticate with Auth0 when Canva authentication fails',
+      localStorageData: null,
       canvaAuth: false,
-      auth0Auth: false,
-      expectedIsAuthenticating: false,
-      getExpectedAuthState: () => ({}),
-    },
-    {
-      name: 'when local storage is empty and Canva authentication succeeds but Auth0 fails, should not be authenticating',
-      authState: null,
-      canvaAuth: true,
-      auth0Auth: false,
-      expectedIsAuthenticating: false,
-      getExpectedAuthState: () => ({}),
-    },
-    {
-      name: 'when local storage is empty, Canva authentication succeeds and Auth0 authentication succeeds',
-      authState: null,
-      canvaAuth: true,
-      auth0Auth: true,
-      expectedIsAuthenticating: false,
-      getExpectedAuthState: (auth0Token) => ({ auth: { isAuthenticated: true, data: { token: auth0Token } } }),
-    },
-    {
-      name: 'when local storage is empty, Canva authentication succeeds and Auth0 authentication fails',
-      authState: null,
-      canvaAuth: true,
-      auth0Auth: false,
-      expectedIsAuthenticating: false,
-      getExpectedAuthState: () => ({}),
-    },
+      isAuthenticating: false
+    }
   ];
 
-  // For each test case...
-  testCases.forEach((testCase) => {
-    // Declare the test and its name
-    it(testCase.name, async () => {
-      // Mock a function to update the state
-      const updateState = jest.fn();
-      // Mock the return value of useAppState to return the mock function
-      (useAppState as jest.Mock).mockReturnValue(
-        {updateState,}
-      );
-      // Mock the return value of useAuth0Auth to simulate verification of the Auth0 token
-      const auth0Token = testCase.auth0Auth ? 'auth0Token' : null;
-      (useAuth0Auth as jest.Mock).mockReturnValue(
-        {verifyAuth0Token: jest.fn().mockResolvedValue(auth0Token)}
-      );
+  testCases.forEach(({ description, localStorageData, canvaAuth, isAuthenticating }) => {
+    it(description, () => {
+      const mockUpdateState = jest.fn();
+      const mockVerifyAuth0Token = jest.fn();
 
-      // If the test case has an auth state, set it in the localStorage
-      if (testCase.authState) {
-        setAuthStateInLocalStorage(testCase.authState);
+      (useAppState as jest.Mock).mockReturnValue({
+        updateState: mockUpdateState
+      });
+
+      (useAuth0Auth as jest.Mock).mockReturnValue({
+        verifyAuth0Token: mockVerifyAuth0Token
+      });
+
+      (initiateCanvaAuthenticationFlow as jest.Mock).mockResolvedValue(canvaAuth);
+
+      mockLocalStorage.getItem.mockReturnValue(localStorageData ? JSON.stringify(localStorageData) : null);
+
+      const { result } = renderHook(() => useAuthentication());
+
+      // Arrange
+      expect(result.current.isAuthenticating).toBe(true);
+
+      // Act
+      act(() => {
+        jest.runAllTimers();
+      });
+
+      // Assert
+      expect(result.current.isAuthenticating).toBe(isAuthenticating);
+      if (localStorageData) {
+        expect(mockUpdateState).toHaveBeenCalledWith({ auth: localStorageData });
+      } else if (canvaAuth) {
+        expect(mockVerifyAuth0Token).toHaveBeenCalled();
+      } else {
+        expect(mockVerifyAuth0Token).not.toHaveBeenCalled();
       }
-
-      // Mock the Canva authentication function to return the canvaAuth property of the test case
-      (initiateCanvaAuthenticationFlow as jest.Mock).mockResolvedValue(testCase.canvaAuth);
-
-      // Render the TestComponent
-      const { getByText } = render(<TestComponent />);
-
-      // Wait for the authenticating status to match the expected value
-      await waitFor(() => getByText(`Authenticating: ${testCase.expectedIsAuthenticating}`));
-
-      // Check that the authentication state matches the expected value
-      const expectedAuthState = testCase.getExpectedAuthState(auth0Token);
-      expect(getByText(`Auth State: ${JSON.stringify(expectedAuthState)}`)).toBeDefined();
     });
   });
 });
-
