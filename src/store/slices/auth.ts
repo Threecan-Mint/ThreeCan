602aacab001247f1f8ba75fe3c5a3f9daa0f4e3b
Hewlbern <michael.t.holborn@gmail.com>
2023-07-27 16:13:17 +1000

    auth

--- src/store/slices/auth.ts
+++ src/store/slices/auth.ts
@@ -3,27 +3,29 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 // types
 import type { Auth } from "src/types";
 
-// initial state
+// Define the initial state for the auth slice
 const initialState: Auth = {
   isAuthenticated: false,
   isLoggedOut: false,
   data: {},
 };
 
-// slice
+// Define the auth slice
 const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
+    // Set the auth state
     setAuth: (_state, action: PayloadAction<Auth>) => {
       return { ...action.payload, isLoggedOut: false };
     },
+    // Log out by resetting the auth state to its initial value
     logOut: () => {
       return { ...initialState, isLoggedOut: true };
     },
   },
 });
 
-// actions
+// Export the actions and the reducer
 export const { setAuth, logOut } = authSlice.actions;
 export default authSlice.reducer;
